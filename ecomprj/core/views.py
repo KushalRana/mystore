95410d6c141fd49f88a5f1d21dfcae28ab531938
kushal rana <kushalrana1420@gmail.com>
2023-09-30 05:12:41 +0545

    made categories

--- ecomprj/core/views.py
+++ ecomprj/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from core.models import *
+from core.models import Product, Category
 
 # Create your views here.
 
@@ -13,3 +13,12 @@ def index(request):
     }
 
     return render(request, 'core/index.html', context)
+
+
+def category_list_view(request):
+    categories = Category.objects.all()
+    context = {
+        "categories": categories,
+    }
+
+    return render(request, 'core/category-list.html', context)
